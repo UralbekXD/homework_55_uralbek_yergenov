1019a6ecba7e52d4f1781771b64f58f9b8105cc7
Uralbek Yergenov <oralbekergenov@gmail.com>
2023-02-14 16:28:20 +0600

    Add create_task view

--- src/todolist/views.py
+++ src/todolist/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from todolist.models import Task
 
@@ -22,3 +22,25 @@ def view_task(request):
 
     return render(request, 'task.html', context=context)
 
+
+def create_task(request):
+    match request.method:
+        case 'GET':
+            context = {
+                'status_choices': Task.STATUS_CHOICES,
+            }
+            return render(request, 'create.html', context=context)
+        case 'POST':
+            title = request.POST.get('title')
+            description = request.POST.get('description')
+            status = request.POST.get('status')
+            deadline = request.POST.get('deadline')
+
+            task = Task.objects.create(
+                title=title,
+                description=description,
+                status=status,
+                deadline=deadline,
+            )
+
+            return redirect('/task/?pk={primary_key}'.format(primary_key=task.pk))
