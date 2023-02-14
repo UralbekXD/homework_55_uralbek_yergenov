fb377fc232501f10a9c834066c3440d3a5b9fae5
Uralbek Yergenov <oralbekergenov@gmail.com>
2023-02-14 15:06:22 +0600

    Create index view

--- src/todolist/views.py
+++ src/todolist/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from todolist.models import Task
+
+
 # Create your views here.
+def index(request):
+    tasks = Task.objects.all()
+    context = {
+        'tasks': tasks
+    }
+
+    return render(request, 'index.html', context=tasks)
