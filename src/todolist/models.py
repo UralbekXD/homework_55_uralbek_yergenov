a8159f03a4e178f5e7251bf4a41bc1047add9621
Uralbek Yergenov <oralbekergenov@gmail.com>
2023-02-14 14:46:04 +0600

    Add task model

--- src/todolist/models.py
+++ src/todolist/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=128, null=False, blank=False, verbose_name='Заголовок')
+    description = models.TextField(max_length=4096, null=False, blank=False, verbose_name='Описание')
+
+    NEW = 'new'
+    IN_PROGRESS = 'in progress'
+    DONE = 'done'
+    STATUS_CHOICES = [
+        (NEW, 'Новый'),
+        (IN_PROGRESS, 'В процессе'),
+        (DONE, 'Сделано'),
+    ]
+    status = models.CharField(
+        max_length=16,
+        null=False,
+        blank=False,
+        verbose_name='Статус',
+        choices=STATUS_CHOICES,
+        default=NEW,
+    )
+
+    deadline = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.title} {self.status} {self.deadline}'
