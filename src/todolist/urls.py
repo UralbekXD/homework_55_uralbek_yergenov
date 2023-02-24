f036d15b7e267d82faf0c39da0b2d6e85196782a
Uralbek Yergenov <oralbekergenov@gmail.com>
2023-02-24 16:54:09 +0600

    Добавить мозможность удаления и редактирования через Django формы

--- src/todolist/urls.py
+++ src/todolist/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', index, name='index'),
     path('task/<int:pk>', view_task, name='task_view'),
     path('task/add/', create_task, name='task_create'),
+    path('task/<int:pk>/edit/', update_task, name='task_update'),
+    path('task/<int:pk>/delete/', delete_task, name='task_delete'),
 ]
--- src/todolist/views.py
+++ src/todolist/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 
 from datetime import datetime
-from todolist.models import Task
+from .models import Task, STATUS_CHOICES
+from .forms import TaskForm
 
 
 # Create your views here.
@@ -25,24 +26,57 @@ def view_task(request, pk):
 def create_task(request):
     match request.method:
         case 'GET':
-            context = {
-                'status_choices': Task.STATUS_CHOICES,
+            form = TaskForm()
+            return render(request, 'create.html', context={
+                'form': form,
+                'status_choices': STATUS_CHOICES,
                 'time_now': datetime.today().strftime('%Y-%m-%d'),
-            }
-            return render(request, 'create.html', context=context)
+            })
+
         case 'POST':
-            title = request.POST.get('title')
-            description = request.POST.get('description')
-            status = request.POST.get('status')
-            deadline = request.POST.get('deadline')
-            full_description = request.POST.get('full_description')
+            form = TaskForm(data=request.POST)
+            if not form.is_valid():
+                return render(request, 'create.html', context={
+                    'form': form,
+                    'status_choices': STATUS_CHOICES,
+                    'time_now': datetime.today().strftime('%Y-%m-%d'),
+                })
 
-            task = Task.objects.create(
-                title=title,
-                description=description,
-                status=status,
-                deadline=deadline,
-                full_description=full_description,
-            )
-
+            task = Task.objects.create(**form.cleaned_data)
             return redirect(reverse('task_view', kwargs={'pk': task.pk}))
+
+
+def update_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    match request.method:
+        case 'GET':
+            form = TaskForm(
+                data={
+                    'title': task.title,
+                    'description': task.description,
+                    'full_description': task.full_description,
+                    'status': task.status,
+                    'deadline': task.deadline,
+                }
+            )
+            return render(request, 'edit.html', context={
+                'pk': task.pk,
+                'form': form,
+            })
+
+        case 'POST':
+            form = TaskForm(data=request.POST)
+            if not form.is_valid():
+                return render(request, 'edit.html', context={
+                    'pk': task.pk,
+                    'form': form,
+                })
+
+            return redirect(reverse('task_view', kwargs={'pk': pk}))
+
+
+def delete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect('index')
+
