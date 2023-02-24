40696e60cc482bebb452be019ceb13958a329b1b
Uralbek Yergenov <oralbekergenov@gmail.com>
2023-02-24 15:27:22 +0600

    Reformat file, Fix create task from title

--- src/todolist/models.py
+++ src/todolist/models.py
@@ -1,20 +1,22 @@
 from django.db import models
 
 
+NEW = 'new'
+IN_PROGRESS = 'in progress'
+DONE = 'done'
+STATUS_CHOICES = [
+    (NEW, 'Новый'),
+    (IN_PROGRESS, 'В процессе'),
+    (DONE, 'Сделано'),
+]
+
+
 # Create your models here.
 class Task(models.Model):
     title = models.CharField(max_length=128, null=False, blank=False, verbose_name='Заголовок')
     description = models.CharField(max_length=128, null=False, blank=False, verbose_name='Описание')
     full_description = models.TextField(max_length=4096, null=True, blank=True, verbose_name='Полное описание')
 
-    NEW = 'new'
-    IN_PROGRESS = 'in progress'
-    DONE = 'done'
-    STATUS_CHOICES = [
-        (NEW, 'Новый'),
-        (IN_PROGRESS, 'В процессе'),
-        (DONE, 'Сделано'),
-    ]
     status = models.CharField(
         max_length=16,
         null=False,
